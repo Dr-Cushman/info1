baffc0321528fc02b12a01e6956b28ffa39cda37
Leon Romanenko <leon.romanenko@icloud.com>
2023-12-19 04:05:20 +0100

    Add random number generation to SupportTest.java an initialized two random numbers in two ways for practice

--- chapter06-ws2023/tech-support1/SupportTest.java
+++ chapter06-ws2023/tech-support1/SupportTest.java
@@ -1,8 +1,18 @@
+import java.util.Random;
+
 public class SupportTest {
     public static void main(String[] args) {
         // Create a support system and start it
         SupportSystem supportSystem = new SupportSystem();
         supportSystem.start(); 
+
+        // Create a random object and assign a random int 
+        Random random = new Random();
+        int randomNumber = random.nextInt(100);
+
+        // Assign a random number to a integer variable in one line
+        // Creates a random object while assinging it to randomNumber2
+        int randomNumber2 = new Random().nextInt(100);
 
     }
 }
