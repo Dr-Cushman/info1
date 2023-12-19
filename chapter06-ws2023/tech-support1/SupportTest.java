496d76a96fbf7b59ff6b295feb2413ea74f26bcb
Leon Romanenko <leon.romanenko@icloud.com>
2023-12-19 03:23:35 +0100

    Add support system and test InputReader and Responder methods

--- chapter06-ws2023/tech-support1/SupportTest.java
+++ chapter06-ws2023/tech-support1/SupportTest.java
@@ -1,7 +1,19 @@
 public class SupportTest {
     public static void main(String[] args) {
-        SupportSystem supportSystem = new SupportSystem();
+        // Create a support system and start it
+        // SupportSystem supportSystem = new SupportSystem();
+        // supportSystem.start();
 
-        supportSystem.start();
+        // Generate a InputReader
+        InputReader reader = new InputReader();
+        // Test InputReader method
+        String input = reader.getInput();
+        System.out.println(input);
+
+        // Generate a Responder
+        Responder responder = new Responder();
+        // Test Responder method
+        String response = responder.generateResponse();
+        System.out.println(response);
     }
 }
