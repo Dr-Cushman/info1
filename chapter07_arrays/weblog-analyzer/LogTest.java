dbd3943b31537d5d77891185701dda497a072f04
Leon Romanenko <leon.romanenko@icloud.com>
2023-12-26 18:55:39 +0200

    git rm --cached chapter07_arrays

---
+++ chapter07_arrays/weblog-analyzer/LogTest.java
@@ -0,0 +1,7 @@
+public class LogTest {
+    public static void main(String[] args) {
+        LogAnalyzer analyzer = new LogAnalyzer();
+        analyzer.analyzeHourlyData();
+        analyzer.printHourlyCounts();
+    }
+}
